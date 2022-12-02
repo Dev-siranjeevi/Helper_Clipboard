4ec6ec0da0dd8560ee789092486e53576e8c524b
Dev-siranjeevi <you@example.com>
2022-12-02 08:57:59 +0530

    UI/UX update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-const {
-  ipcRenderer,
-  clipboard
-} = require("electron");
+const { ipcRenderer, clipboard } = require("electron");
 
-import {
-  copyToClipboard,
-  clipList
-} from "./components/clipboard.js"
+import { copyToClipboard, clipList } from "./components/clipboard.js";
 
 setInterval(() => {
   copyToClipboard();
 }, 800); //Run it ever 500 ms
 
-const pinApplication = document.querySelector('#move');
+const pinApplication = document.querySelector("#move");
 pinApplication.addEventListener("click", () => {
-  ipcRenderer.send("Update-Application-position", "Toogle Applications mobility");
+  ipcRenderer.send(
+    "Update-Application-position",
+    "Toogle Applications mobility"
+  );
   // Set styling
-  pinApplication.className.baseVal.includes("pinactive") ? pinApplication.classList.remove("pinactive") : pinApplication.classList.add("pinactive");
-})
+  pinApplication.className.baseVal.includes("pinactive")
+    ? pinApplication.classList.remove("pinactive")
+    : pinApplication.classList.add("pinactive");
+});
 
-const alwaysOntop = document.querySelector('#layer');
+const alwaysOntop = document.querySelector("#layer");
 alwaysOntop.addEventListener("click", () => {
   ipcRenderer.send("Update-Application-layer", "Toogle Applications mobility");
   // Set styling
-  alwaysOntop.className.baseVal.includes("pinactive") ? alwaysOntop.classList.remove("pinactive") : alwaysOntop.classList.add("pinactive");
-})
+  alwaysOntop.className.baseVal.includes("pinactive")
+    ? alwaysOntop.classList.remove("pinactive")
+    : alwaysOntop.classList.add("pinactive");
+});
